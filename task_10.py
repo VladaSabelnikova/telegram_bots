d66158a40e779b9d96039580fd55aa6fc7b9831e
vlada <vladasabelnikova@gmail.com>
2022-02-18 20:11:25 +0300

    Десятая задача решена
    Всё

---
+++ task_10.py
@@ -0,0 +1,69 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, \
+    ConversationHandler
+
+load_dotenv()
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+address = 'https://api.mymemory.translated.net/get?q={0}!&langpair={1}'
+langpair = ''
+
+
+def start(update, context):
+    reply_keyboard = [['ru|en', 'en|ru']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    update.message.reply_text(
+        'Привет! Я бот-переводчик.\nС какого на какой язык мне переводить?',
+        reply_markup=markup
+    )
+    return 1
+
+
+def get_langpair(update, context):
+    global langpair
+    langpair = update.message.text
+    update.message.reply_text('Хорошо, а что мне переводить?')
+    return 2
+
+
+def translator(update, context):
+    text = update.message.text
+    addr = address.format(text, langpair)
+
+    response = requests.get(addr)
+    if response:
+        json_response = response.json()
+        result = json_response['responseData']['translatedText']
+        update.message.reply_text(result)
+
+    return 2
+
+
+def stop(update, context):
+    update.message.reply_text('Всего доброго')
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            1: [MessageHandler(Filters.text, get_langpair)],
+            2: [MessageHandler(Filters.text, translator)],
+        },
+
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
